6de97303f0864659e7cab64b8663bf7b58464b2e
DaehyunKwon <kdh_blue@hanmail.net>
2014-07-25 20:50:52 +0900

    외부 라이브러리 포함

--- src/openuasl/resq/android/activity/QRCodeActivity.java
+++ src/openuasl/resq/android/activity/QRCodeActivity.java
@@ -7,24 +7,27 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.zxing.client.android.CaptureActivity;
+
 //import com.google.zxing.client.android.Result;
 public class QRCodeActivity extends CaptureActivity {
-	  public static final String EXT_BARCODE = "EXT_BARCODE";
-	  public static String qrvalueresult = new String();
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	    }
-	 
-	    public void handleDecode(com.google.zxing.Result rawResult, 
-	    		Bitmap barcode, float scaleFactor) {
-	         super.handleDecode(rawResult, barcode, scaleFactor);
-	       setResult(RESULT_OK, new Intent().putExtra(EXT_BARCODE, rawResult.getText()));
-	    	Toast.makeText(this.getApplicationContext(), "Scanned code "+rawResult.getText(), Toast.LENGTH_LONG);
-	    	Log.i("qrcode",rawResult.getText());
-	    	qrvalueresult=rawResult.getText();
-	    	
-	    	
-	    	finish();
-	    }
+	public static final String EXT_BARCODE = "EXT_BARCODE";
+	public static String qrvalueresult = new String();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	public void handleDecode(com.google.zxing.Result rawResult, Bitmap barcode,
+			float scaleFactor) {
+		super.handleDecode(rawResult, barcode, scaleFactor);
+		setResult(RESULT_OK,
+				new Intent().putExtra(EXT_BARCODE, rawResult.getText()));
+		Toast.makeText(this.getApplicationContext(), "Scanned code "
+				+ rawResult.getText(), Toast.LENGTH_LONG);
+		Log.i("qrcode", rawResult.getText());
+		qrvalueresult = rawResult.getText();
+
+		finish();
+	}
 }
